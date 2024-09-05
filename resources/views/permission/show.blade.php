dc48b3b607dca8b0a4e214ffa7eba9df9709b5eb
Josue Chevez <123977049+josuechevezUEES@users.noreply.github.com>
2024-09-05 17:57:54 -0600

    fix view route

--- resources/views/permission/show.blade.php
+++ resources/views/permission/show.blade.php
@@ -3,38 +3,40 @@
 @section('title', 'Show Permission')
 
 @section('content_header')
-    <h1>{{ __('Show') }} {{ __('Permission') }}</h1>
+    <h1 class="m-0 text-dark">{{ __('Show') }} {{ __('Permission') }}</h1>
 @stop
 
 @section('content')
-    <section class="content container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header" style="display: flex; justify-content: space-between; align-items: center;">
-                        <div class="float-left">
-                            <span class="card-title">{{ __('Show') }} Permission</span>
-                        </div>
-                        <div class="float-right">
-                            <a class="btn btn-primary btn-sm" href="{{ route('permissions.index') }}">
-                                {{ __('Back') }}</a>
-                        </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header bg-dark">
+                    <h3 class="card-title">{{ __('Permission Details') }}</h3>
+                    <div class="card-tools">
+                        <a href="{{ route('permissions.index') }}" class="btn btn-secondary btn-sm">
+                            <i class="fas fa-arrow-left"></i> {{ __('Back') }}
+                        </a>
                     </div>
-
-                    <div class="card-body bg-white">
-
-                        <div class="form-group mb-2 mb20">
-                            <strong>Name:</strong>
-                            {{ $permission->name }}
-                        </div>
-                        <div class="form-group mb-2 mb20">
-                            <strong>Guard Name:</strong>
-                            {{ $permission->guard_name }}
-                        </div>
-
+                </div>
+                <div class="card-body">
+                    <div class="form-group">
+                        <label><strong>{{ __('Name') }}:</strong></label>
+                        <p class="text-muted">{{ $permission->name }}</p>
                     </div>
+                    <div class="form-group">
+                        <label><strong>{{ __('Guard Name') }}:</strong></label>
+                        <p class="text-muted">{{ $permission->guard_name }}</p>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <a href="{{ route('permissions.edit', $permission->id) }}" class="btn btn-info btn-sm">
+                        <i class="fas fa-edit"></i> {{ __('Edit') }}
+                    </a>
+                    <a href="{{ route('permissions.index') }}" class="btn btn-default btn-sm">
+                        <i class="fas fa-list"></i> {{ __('View All Permissions') }}
+                    </a>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
 @endsection

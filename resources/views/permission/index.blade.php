98374943cde143fa918323433df53b41be002b5a
Josue Chevez <123977049+josuechevezUEES@users.noreply.github.com>
2024-09-05 17:46:19 -0600

    fix view

--- resources/views/permission/index.blade.php
+++ resources/views/permission/index.blade.php
@@ -19,11 +19,12 @@
                                 {{ __('Permissions') }}
                             </span>
 
-                             <div class="float-right">
-                                <a href="{{ route('permissions.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
-                                  {{ __('Create New') }}
+                            <div class="float-right">
+                                <a href="{{ route('permissions.create') }}" class="btn btn-primary btn-sm float-right"
+                                    data-placement="left">
+                                    {{ __('Create New') }}
                                 </a>
-                              </div>
+                            </div>
                         </div>
                     </div>
                     @if ($message = Session::get('success'))
@@ -38,9 +39,9 @@
                                 <thead class="thead">
                                     <tr>
                                         <th>No</th>
-                                        
-									<th >Name</th>
-									<th >Guard Name</th>
+
+                                        <th>Name</th>
+                                        <th>Guard Name</th>
 
                                         <th></th>
                                     </tr>
@@ -49,17 +50,24 @@
                                     @foreach ($permissions as $permission)
                                         <tr>
                                             <td>{{ ++$i }}</td>
-                                            
-										<td >{{ $permission->name }}</td>
-										<td >{{ $permission->guard_name }}</td>
+
+                                            <td>{{ $permission->name }}</td>
+                                            <td>{{ $permission->guard_name }}</td>
 
                                             <td>
-                                                <form action="{{ route('permissions.destroy', $permission->id) }}" method="POST">
-                                                    <a class="btn btn-sm btn-primary " href="{{ route('permissions.show', $permission->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('permissions.edit', $permission->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
+                                                <form action="{{ route('permissions.destroy', $permission->id) }}"
+                                                    method="POST">
+                                                    <a class="btn btn-sm btn-primary "
+                                                        href="{{ route('permissions.show', $permission->id) }}"><i
+                                                            class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
+                                                    <a class="btn btn-sm btn-success"
+                                                        href="{{ route('permissions.edit', $permission->id) }}"><i
+                                                            class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm" onclick="event.preventDefault(); confirm('Are you sure to delete?') ? this.closest('form').submit() : false;"><i class="fa fa-fw fa-trash"></i> {{ __('Delete') }}</button>
+                                                    <button type="submit" class="btn btn-danger btn-sm"
+                                                        onclick="event.preventDefault(); confirm('Are you sure to delete?') ? this.closest('form').submit() : false;"><i
+                                                            class="fa fa-fw fa-trash"></i> {{ __('Delete') }}</button>
                                                 </form>
                                             </td>
                                         </tr>

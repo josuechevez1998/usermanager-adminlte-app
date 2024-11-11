4247e67b3765c0da2b31c56b7e02f8474c3c04d1
josuechevez1998 <josuechevez977@gmail.com>
2024-11-10 21:48:46 -0600

    feat: extends blade

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,33 +1,44 @@
-@extends('layouts.app')
+@extends('adminlte::page')
 
-@section('template_title')
-    {{ $user->name ?? __('Show') . " " . __('User') }}
-@endsection
+@section('title', $user->name)
+
+@section('content_header')
+    <h1>
+        {{ __('Show') }}
+        {{ $user->name }}
+    </h1>
+@stop
 
 @section('content')
     <section class="content container-fluid">
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header" style="display: flex; justify-content: space-between; align-items: center;">
-                        <div class="float-left">
-                            <span class="card-title">{{ __('Show') }} User</span>
-                        </div>
-                        <div class="float-right">
-                            <a class="btn btn-primary btn-sm" href="{{ route('users.index') }}"> {{ __('Back') }}</a>
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-10 col-md-10 col-sm-10">
+                                <span class="card-title">
+                                    {{ __('Show') }}
+                                    {{ $user->name  }}
+                                </span>
+                            </div>
+                            <div class="col-2 col-md-2 col-sm-2">
+                                <a class="btn btn-primary btn-sm btn-block" href="{{ route('users.index') }}">
+                                    {{ __('Back') }}
+                                </a>
+                            </div>
                         </div>
                     </div>
 
                     <div class="card-body bg-white">
-                        
-                                <div class="form-group mb-2 mb20">
-                                    <strong>Name:</strong>
-                                    {{ $user->name }}
-                                </div>
-                                <div class="form-group mb-2 mb20">
-                                    <strong>Email:</strong>
-                                    {{ $user->email }}
-                                </div>
+                        <div class="form-group mb-2 mb20">
+                            <strong>{{ __('Name') }}:</strong>
+                            {{ $user->name }}
+                        </div>
+                        <div class="form-group mb-2 mb20">
+                            <strong>{{ __('Email') }}:</strong>
+                            {{ $user->email }}
+                        </div>
 
                     </div>
                 </div>
